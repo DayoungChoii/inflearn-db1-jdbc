c6f14ca752ed9bdde37544a2f1b509374d15d4a5
DayoungChoii <young12430@gmail.com>
2022-06-22 22:42:45 +0900

    [spring db1] JDBC 개발 - 조회

--- src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryVOTest.java
@@ -1,12 +1,15 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
+@Slf4j
 class MemberRepositoryVOTest {
 
     MemberRepositoryVO repository = new MemberRepositoryVO();
@@ -15,6 +18,13 @@ class MemberRepositoryVOTest {
     void curd() throws SQLException {
         Member member = new Member("memberV2", 10000);
         repository.save(member);
+
+        //findById
+        Member findMember = repository.findById(member.getMemberId());
+        log.info("findMember={}", findMember);
+        assertThat(findMember).isEqualTo(member);
     }
+
+
 
 }
